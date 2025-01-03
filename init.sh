524661aa06671fd6c0eb32b4829a5b48e98650b1
simsononroad <d-github@kopaszhegy.hu>
2025-01-03 23:22:16 +0100

    cleanup: remove easy_sqlite_db directory after migration to easy_db

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ echo "Installing easy_db Python library"
 git clone https://github.com/simsononroad/easy_sqlite_db.git
 cd easy_sqlite_db
 mv easy_db ../
+rm -r easy_sqlite_db
 echo "from easy_db import *" > main.py
