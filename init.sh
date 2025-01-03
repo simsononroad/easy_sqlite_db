9957dc5083b9263af02c5292a7bb7418b1256bdf
simsononroad <d-github@kopaszhegy.hu>
2025-01-03 23:32:08 +0100

    refactor: update init script to include Hungarian messages and prompt for sudo password

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-echo "Installing easy_db Python library"
+echo "Letöltés: easy_db Python library"
 git clone https://github.com/simsononroad/easy_sqlite_db.git
 cd easy_sqlite_db
 mv easy_db ../
 cd ..
+echo "A jelszó a felesleges mappa törléséhez szükséges!"
 sudo rm -r easy_sqlite_db
 cat > main.py << EOF
 from easy_db import *
