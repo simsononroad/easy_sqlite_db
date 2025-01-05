10f8effc431516e1d80c5b8e4a3af16ca150d7ac
simsononroad <d-github@kopaszhegy.hu>
2025-01-05 18:07:09 +0100

    setup update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import codecs
 import os
-VERSION = '0.4'
+VERSION = '1.0'
 DESCRIPTION = 'An easy database package'
 LONG_DESCRIPTION = 'A package that based on sqlite'
 
