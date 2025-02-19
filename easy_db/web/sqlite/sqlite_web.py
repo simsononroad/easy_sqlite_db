5f5a85dd6829050935ac9842d7b10b376be4d4e7
simsononroad <d-github@kopaszegy.hu>
2025-02-19 19:47:26 +0100

    create index page(v0.1)

--- easy_db/web/sqlite/sqlite_web.py
+++ easy_db/web/sqlite/sqlite_web.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, jsonify
 
+
 def start_website(port=5000):
     app = Flask(__name__)
-    
     @app.route("/")
     def index():
-        return "<h1>Hello World</h1>"
-    
-    if __name__=="_main__":
-        app.run(port=port)
+        return render_template("index.html")
+    return app
